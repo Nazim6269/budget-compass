3e2829aa02957f7d5bb0277af450f1dc1569c8b4
Nazim6269 <nazimdev10022001@gmail.com>
2026-04-25 17:47:18 +0600

    feat: add navigation from searhing

--- widgets/topbar/ui/TopBar.tsx
+++ widgets/topbar/ui/TopBar.tsx
@@ -3,15 +3,27 @@
 import { GenericSearch } from "@/shared/ui/GenericSearch";
 import Notification from "@/shared/ui/icons/Notification";
 import Image from "next/image";
+import { NAV_CONFIG } from "../../sidebar/config/navigation";
+import { useRouter } from "next/navigation";
+
+function searchNav(query: string) {
+  const result = NAV_CONFIG.flatMap((group) => group.items).filter((item) =>
+    item.label.toLowerCase().includes(query.toLowerCase()),
+  );
+  return result;
+}
 
 export const Topbar = () => {
+  const router = useRouter();
   return (
     <header className="w-full border-b border-lineBorder flex items-center justify-between gap-4 p-6">
       {/* Left - Search */}
-      <div className="flex-1">
+      <div className="flex-1 hidden sm:flex">
         <GenericSearch
-          onSelect={() => {}}
-          onSearch={() => []}
+          onSelect={(item: any) => {
+            router.push(item.href);
+          }}
+          onSearch={(query: string) => searchNav(query)}
           className="max-w-md"
           placeholder="Quick Search..."
           size="lg"
